ad4d02ceb507cbf0de0dd991b3005030e976cd16
Пользователь <d.elllka@bk.ru>
2023-02-14 16:12:23 +0300

    задачи

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,45 @@ public class Main {
         paper = paper +4;
         System.out.println(paper);
         paper = paper - 7639;
+
         System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var boxersWeight1 = 78.2;
+        var boxersWeight2 = 82.7;
+        var totalWeight = boxersWeight1 + boxersWeight2;
+        System.out.println("Общий вес двух бойцов " + totalWeight + " кг.");
+
+        var weightDifference = boxersWeight2 - boxersWeight1;
+        System.out.println("Разница весов спортсменов " + weightDifference);
+
+        var weightDifference1 = (boxersWeight2 / boxersWeight1) %totalWeight;
+        System.out.println("Разница весов спортсменов с помощью функции остаток деления " + weightDifference);
+
+        var officeHours = 640;
+        var employeesWorkingHours = 8;
+        var numbersEmployess = officeHours / employeesWorkingHours;
+        System.out.println("Всего работников в компании - " + numbersEmployess + " человек");
+
+        numbersEmployess = numbersEmployess + 94;
+        officeHours = officeHours + (94 * 8);
+        System.out.println("Если в компании работает " + numbersEmployess  +" человек, то всего " + officeHours + " часов работы может быть поделено между сотрудниками");
+
+
+
     }
 }
